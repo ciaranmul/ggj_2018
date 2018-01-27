12b9fd8fdbbc574ebe7d9afd0cefa7e894d4312c
Padraig Fitzpatrick <padraig.fitzpatrick1@gmail.com>
2018-01-27 14:48:02 +0000

    Adding turntable prefab, slider script and record script

---
+++ Assets/Scripts/Turntable/RecordMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordMovement : MonoBehaviour {
+
+    float rotationSpeed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationSpeed = Time.deltaTime * 30;
+        transform.Rotate(0, rotationSpeed, 0, Space.Self);
+    }
+}
---
+++ Assets/Scripts/Turntable/SliderMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class SliderMovement : MonoBehaviour {
+
+
+    Vector3 newPosition;
+    void Start()
+    {
+        newPosition = transform.position;
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= 0.002f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x += 0.002f;
+            this.transform.position = position;
+        }
+     
+    }
+
+
+}
+
