714d8b8a88db5cfa1a70bebb1c75cf1419be79ee
Ciaran Mulholland <ciaranmul@googlemail.com>
2018-01-27 10:15:20 +0000

    Add player and controller
    - Adds player and 2dcontroller scripts
    - Adds player controls, move and jump
        - Need to approximate speeds to multiples of 2
    - Adds player collision handling

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2d))]
+public class Player : MonoBehaviour {
+
+    Controller2d controller;
+
+    Vector3 velocity;
+    public float moveSpeed = 10;
+    public float gravity = -20;
+    public float jumpVelocity = 10;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<Controller2d>();
+        velocity.x = Mathf.Clamp(0, -10, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if(controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
+        {
+            velocity.y = jumpVelocity;
+        }
+
+        velocity.x += input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+	}
+}
