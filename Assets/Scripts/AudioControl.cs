29733a5302600c1e08a9184391f89f129f34fcf5
Ciaran Mulholland <ciaranmul@googlemail.com>
2018-01-27 20:05:39 +0000

    Adding audio tempo changing

---
+++ Assets/Scripts/AudioControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControl : MonoBehaviour {
+
+    AudioSource audioSource;
+    public int startingPitch = 1;
+
+    // Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.pitch = startingPitch;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        audioSource.pitch = GameObject.Find("Slider").transform.localPosition.x;
+
+	}
+}
