2216380f068691641d3a8bb839f75c405000faaf
Padraig Fitzpatrick <padraig.fitzpatrick1@gmail.com>
2018-01-27 15:41:23 +0000

    Added record spin and bounds on slider movement

--- Assets/Scripts/Turntable/RecordMovement.cs
+++ Assets/Scripts/Turntable/RecordMovement.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class RecordMovement : MonoBehaviour {
 
     float rotationSpeed;
+    SliderMovement sliderScript;
+    GameObject slider;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        GameObject slider = GameObject.Find("Slider");
+        SliderMovement sliderScript = slider.GetComponent<SliderMovement>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        rotationSpeed = Time.deltaTime * 30;
+        rotationSpeed = Time.deltaTime  * 100 * GameObject.Find("Slider").GetComponent<SliderMovement>().sliderValue;
         transform.Rotate(0, rotationSpeed, 0, Space.Self);
     }
 }
--- Assets/Scripts/Turntable/SliderMovement.cs
+++ Assets/Scripts/Turntable/SliderMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 
 public class SliderMovement : MonoBehaviour {
 
+    public float sliderValue = 0;
 
     Vector3 newPosition;
     void Start()
@@ -17,14 +18,25 @@ public class SliderMovement : MonoBehaviour {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             Vector3 position = this.transform.position;
-            position.x -= 0.002f;
-            this.transform.position = position;
+
+            if (position.x - 0.0002f > -1f)
+            {
+                position.x -= 0.02f;
+                sliderValue = position.x;
+                this.transform.position = position;
+            }
+
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             Vector3 position = this.transform.position;
-            position.x += 0.002f;
-            this.transform.position = position;
+            if(position.x + 0.0002f < 1f)
+            {
+                position.x += 0.02f;
+                sliderValue = position.x;
+                this.transform.position = position;
+            }
+            
         }
      
     }
